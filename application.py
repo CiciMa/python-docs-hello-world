3a5111f6e89ca356bd76f1c3e415044b1ae9d5f2
CiciMa <xxmdaily@gmail.com>
2019-04-25 13:56:07 -0400

    update user input template

--- application.py
+++ application.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import config_cosmos
+import pydocumentdb.document_client as document_client
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    app = "<html><body><h1>Hello Xitang</h1><div>This is test!</div></body></html>"
-    return app
+def homepage():
+    # app = "<html><body><h1>Hello Xitang</h1><div>This is test!</div></body></html>"
+    # return app
+#    dict = {'phy':50,'che':60,'maths':70}
+   return render_template('index.html')
+
+@app.route("/user_data", methods=['POST'])
+def user_data():
+    data = request.form
+    print("----sending user data---")
+    print(data)
+    return render_template('prediction.html', data=data)
+if __name__ == '__main__':
+   app.run(debug = True)
---
+++ config_cosmos.py
@@ -0,0 +1,9 @@
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+COSMOSDB_HOST = 'https://YOUR_COSMOSDB_NAME.documents.azure.com:443/'
+COSMOSDB_KEY = 'YOUR_SECRET_KEY_ENDING_IN_=='
+
+COSMOSDB_DATABASE = 'voting database'
+COSMOSDB_COLLECTION = 'voting collection'
+COSMOSDB_DOCUMENT = 'voting document'
