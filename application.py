d15fc90378d3c1ce6f34460eb9af36457927c2db
CiciMa <xxmdaily@gmail.com>
2019-04-27 12:48:48 -0400

    get data from database

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+import logging
+# import azure.functions as func
 import config_cosmos
 import pydocumentdb.document_client as document_client
+import config_cosmos
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,6 +18,26 @@ def user_data():
     data = request.form
     print("----sending user data---")
     print(data)
+    get_data_from_cosmodb()
     return render_template('prediction.html', data=data)
+
+def get_data_from_cosmodb():
+    client = document_client.DocumentClient(config_cosmos.COSMOSDB_HOST, {'masterKey': config_cosmos.COSMOSDB_KEY})
+    db_id = 'millksdata'
+    db_query = "select * from r where r.id = '{0}'".format(db_id)
+    db = list(client.QueryDatabases(db_query))[0]
+    db_link = db['_self']
+
+    coll_id = 'cowproduction'
+    coll_query = "select * from r where r.id = '{0}'".format(coll_id)
+    coll = list(client.QueryCollections(db_link, coll_query))[0]
+    coll_link = coll['_self']
+
+    # docs = client.ReadDocuments(coll_link)
+    # print(list(docs))
+    query = { 'query': 'SELECT * FROM server s WHERE s.AnimalID = 5' }    
+    docs = client.QueryDocuments(coll_link, query)
+    print(list(docs))
+
 if __name__ == '__main__':
    app.run(debug = True)
--- config_cosmos.py
+++ config_cosmos.py
@@ -1,9 +1,9 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-COSMOSDB_HOST = 'https://YOUR_COSMOSDB_NAME.documents.azure.com:443/'
-COSMOSDB_KEY = 'YOUR_SECRET_KEY_ENDING_IN_=='
+COSMOSDB_HOST = 'https://milkiscloud.documents.azure.com:443/'
+COSMOSDB_KEY = 'WyLLIIdbWzjpzf4QkkG2DJh0TElv3iGqqQJQIC2WzJ8Cf60v5NCo0OQk11cdPjReHVIyJ0bgcatWbtosDlXk0g=='
 
-COSMOSDB_DATABASE = 'voting database'
-COSMOSDB_COLLECTION = 'voting collection'
+COSMOSDB_DATABASE = 'millksdata'
+COSMOSDB_COLLECTION = 'cowproduction'
 COSMOSDB_DOCUMENT = 'voting document'
